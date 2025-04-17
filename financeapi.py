0b069f7aaf26cee49ac9dadc48f1f189575a8c08
Phan Minh Anh Nguyen <s4882466@student.uq.edu.au>
2025-04-17 00:52:04 +0000

    working api

---
+++ financeapi.py
@@ -0,0 +1,17 @@
+# Function to calculate the P/E ratio of a stock
+def pe_ratio(price, earnings):
+    return price / earnings
+
+# Function to calculate the dividend yield of a stock
+def dividend_yield(dividend, price):
+    return dividend / price
+
+# Function to calculate the earnings per share of a stock
+def eps(net_income, shares_outstanding):
+    return net_income / shares_outstanding
+
+# Function to calculate ROA of a stock
+def roa(net_income, total_assets):
+    return net_income / total_assets
+
+
